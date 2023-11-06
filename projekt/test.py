e2c04e387ea26299b383c9fe75330c0c558e53e4
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2023-11-06 17:41:25 +0100

    Add files via upload

--- projekt/test.py
+++ projekt/test.py
@@ -1,39 +1,77 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
-# Przykładowe efektywności pojedynczych modeli
-efficiency_ada = 0.7  # Efektywność dla AdaBoost (m1)
-efficiency_gb = 0.85  # Efektywność dla Gradient Boosting Machine (m2)
-efficiency_rf = 0.9   # Efektywność dla Random Forest (m3)
+# Ładowanie danych z pliku
+file_path = 'australian.dat'
+data = np.loadtxt(file_path)
 
-# Tworzenie listy skumulowanych efektywności
-cumulative_efficiencies = [
-    efficiency_ada,                         # m1
-    efficiency_ada + efficiency_gb,         # m1+m2
-    efficiency_ada + efficiency_gb + efficiency_rf  # m1+m2+m3
+# Rozdzielanie danych na cechy i etykiety
+X, y = data[:, :-1], data[:, -1]
+
+# Dzielenie danych na zbiory treningowe i testowe
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# Inicjalizacja klasyfikatorów
+ada_boost = AdaBoostClassifier()
+gradient_boosting = GradientBoostingClassifier()
+random_forest = RandomForestClassifier()
+svc = SVC(probability=True)  # Włączamy probability, aby móc obliczyć prawdopodobieństwo
+logistic_regression = LogisticRegression()
+knn = KNeighborsClassifier()
+
+# Lista klasyfikatorów
+classifiers = [
+    ada_boost,
+    gradient_boosting,
+    random_forest,
+    svc,
+    logistic_regression,
+    knn
+]
+classifier_names = [
+    'AdaBoost',
+    'GradientBoosting',
+    'RandomForest',
+    'SVC',
+    'LogisticRegression',
+    'KNN'
 ]
 
-# Tworzenie listy efektywności 'separately for'
-separately_for = [
-    efficiency_ada,     # m1 (AdaBoost)
-    efficiency_gb,      # m2 (Gradient Boosting)
-    efficiency_rf       # m3 (Random Forest)
-]
+# Słownik do przechowywania efektywności
+effectiveness = {}
 
-# Punkty na osi x dla wykresu
-milestones = ['m1', 'm1+m2', 'm1+m2+m3']
+# Trenowanie każdego klasyfikatora i obliczanie efektywności
+for clf, name in zip(classifiers, classifier_names):
+    # Stworzenie potoku: normalizacja danych i klasyfikator
+    pipeline = make_pipeline(StandardScaler(), clf)
+    pipeline.fit(X_train, y_train)
+    predictions = pipeline.predict(X_test)
+    effectiveness[name] = accuracy_score(y_test, predictions)
+
+# Obliczanie efektywności kumulatywnej
+cumulative_scores = np.cumsum(list(effectiveness.values()))
+cumulative_effectiveness = cumulative_scores / np.arange(1, len(effectiveness) + 1)
 
 # Tworzenie wykresu
 plt.figure(figsize=(10, 5))
 
-# Rysowanie wykresu skumulowanego
-plt.plot(milestones, cumulative_efficiencies, marker='o', label='cumulative')
+# Wykres dla efektywności kumulatywnej
+plt.plot(classifier_names, cumulative_effectiveness, marker='o', label='Cumulative Effectiveness')
 
-# Rysowanie wykresu 'separately for'
-plt.plot(milestones, separately_for, marker='o', linestyle='--', label='separately for')
+# Wykres dla efektywności pojedynczych modeli
+plt.plot(classifier_names, list(effectiveness.values()), marker='o', linestyle='--', label='Separate Effectiveness')
 
 # Dodanie tytułów i etykiet
-plt.title('Cumulative and Separate Model Effectiveness')
-plt.xlabel('Milestones')
+plt.title('Model Effectiveness Comparison')
+plt.xlabel('Classifiers')
 plt.ylabel('Effectiveness')
 
 # Dodanie legendy
